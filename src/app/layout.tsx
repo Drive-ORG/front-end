400bc6dabfbd35669b5d56224c4b7b33e34f4ebd
Alireza Shahmoradi <alirezashahmoradi98@gmail.com>
2024-07-05 23:29:31 +0330

    alireza shahmoradi was the only author

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,29 @@
 /* eslint-disable @next/next/no-page-custom-font */
 
-import "@/styles/overrides.scss";
+import '@/styles/overrides.scss';
 
-import { Metadata } from "next";
-import { ReactNode } from "react";
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
-import { Layout } from "@/components/Head";
+import { Layout } from '@/components/Head';
 
 export const metadata: Metadata = {
-  authors: [{ name: "Alireza Shahmoradi" }, { name: "Sara Namdar" }],
-  creator: "Alireza Shahmoradi",
+  authors: [{ name: 'Alireza Shahmoradi' }],
+  creator: 'Alireza Shahmoradi',
   description: `This is a final project for university software testing class.`,
-  title: `DriveOrg`,
+  title: `DriveOrg`
 };
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         {/* Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Croissant+One&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Croissant+One&display=swap'
+          rel='stylesheet'
         />
       </head>
       <body>
